b0b55fbba7cc7ee59956711db2cac1079f5f6ea4
GEORGY <goshana87@mail.ru>
2020-10-05 17:27:11 +0300

    initial project outline: create axios, axios instance and interceptors. Paginatios.

--- src/components/films/film-list.jsx
+++ src/components/films/film-list.jsx
@@ -1,17 +1,64 @@
-import React from 'react';
-import './film-list.css';
+import React from "react";
+import "./film-list.css";
+import * as axios from "axios";
+import { filmIds } from "../film-Ids/film-ids";
+
 class FilmList extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            films: null,
+        };
+    }
+
     componentDidMount() {
-        console.log('hello');
+
+        // Pagination
+
+        const pageItems = 1;
+        const current = 1;
+        const from = current * pageItems - pageItems;
+        const to = current * pageItems;
+        const movies = filmIds.slice(from , to);
+        console.log(movies);
+
+        // Axios
+
+        const instance = axios.create({
+            baseURL: 'http://www.omdbapi.com/'
+        });
+
+        // Axios interseptors
+
+        interceptors(instance);
+
+        function setParams(config) {
+            config.params = {
+                apikey: 'a1213c70'
+            }
+            return config;
+        }
+
+        function getData(response) {
+            return response.data;
+        }
+
+        function interceptors(axios) {
+            axios.interceptors.request.use(setParams);
+            axios.interceptors.response.use(getData);
+        }
+
+        async function getMovies() {
+            const request = movies.map((id) => instance.get(`?i=${id}`));
+            console.log(request);
+            const requestMovie = await Promise.all(request);
+            console.log(requestMovie);
+        }
+        getMovies();
     }
 
     render() {
-        return (
-            <div className="film-list">Hello</div>
-        )
+        return <div className="film-list">Hello</div>;
     }
 }
 
